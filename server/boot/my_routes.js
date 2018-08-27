b7b865c26d6ea6ec874744df460c7bed382f7c91
swaraj <jena.swaraj@gmail.com>
2018-08-27 20:16:46 +0530

    dd

--- server/boot/my_routes.js
+++ server/boot/my_routes.js
@@ -4,11 +4,11 @@ module.exports = function(app) {
   // Install a "/ping" route that returns "pong"
   app.get("/success",function(req,res){
     res.send("success");
-  })
+  });
   // Install a "/ping" route that returns "pong"
   app.get("/error",function(req,res){
     res.status(500)
-    res.render('error', { error: "invalid credentials" })
+    res.send("invalid credentials" )
   })
 
 };
